a36e24a126c598fd3c67c6a4e00fbcab5433c4a3
uchidaEditorial <kouei28poiou@icloud.com>
2022-07-17 08:38:19 +0900

    Update

--- Assets/Script/PiceFowwedScripts.cs
+++ Assets/Script/PiceFowwedScripts.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PiceFowwedScripts : MonoBehaviour
 {
+    public const int Upwalk = 0;
+    public const int DownWalk = 1;
+    public const int LeftWalk = 2;
+    public const int RightWalk = 3;
+    public int _state = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,25 @@ public class PiceFowwedScripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Inputkey();
+        switch(_state)
+        {
+            case Upwalk:
+            {
+               this.GetComponent<Animator>().SetInteger("state", Upwalk);
+               break;
+            }
+            case LeftWalk:
+            {
+                this.GetComponent<Animator>().SetInteger("state",LeftWalk);
+                break;
+            }
+             
+
+        }
+    }
+    public void Inputkey()
+    {
+
     }
 }
